ffeb0e1866a0c3d30d194860531d17d8a13222a3
Ark <>
2023-11-13 23:24:16 +0000

    testing api endpoints

---
+++ trains.py
@@ -0,0 +1,50 @@
+
+import http.client, urllib.request, urllib.parse, urllib.error, base64, argparse, json
+
+
+parser = argparse.ArgumentParser()
+# Adding api key argument
+parser.add_argument("-a", "--apiKey", help = "Api Key")
+parser.add_argument("-s", "--stationCode", help = "Station Code")
+
+
+def getUpcomingTrainInfo(trainInfo):
+    # Parse results
+    print('Passenger trains heading to Huntington on YL line: \n')
+
+    for item in trainInfo['Trains']:
+        print('############')
+        # Get the list and filter out non passenger trains
+        # Check for train on Yellow - YL lines only
+        if(item['DestinationName'] != 'No Passenger' and item['Line'] == 'YL'):
+            print('Arrival Time: in ' +item['Min']+ ' mins'
+            '\n From: '+item['LocationName'],
+            '\n To: ' +item['DestinationName'],
+            '\n Line: '+item['Line'])
+
+    return
+
+
+
+# get api key and station codes
+args = parser.parse_args()
+
+if(args.apiKey and args.stationCode):
+
+    # Request headers
+    headers = {
+        'api_key': args.apiKey,
+    }
+    #URL params
+    params = urllib.parse.urlencode({
+    })
+
+    try:
+        conn = http.client.HTTPSConnection('api.wmata.com')
+        conn.request("GET", "/StationPrediction.svc/json/GetPrediction/%s" % args.stationCode, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+        getUpcomingTrainInfo(json.loads(data))
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
